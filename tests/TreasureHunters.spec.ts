e99901c3344b18d893af75a9809c18e4eede6406
Dmytro Virych <damienissa@yahoo.com>
2025-01-03 18:41:01 +0200

    Implemented referral system

--- tests/TreasureHunters.spec.ts
+++ tests/TreasureHunters.spec.ts
@@ -6,19 +6,23 @@ import { TreasureHunters } from '../wrappers/TreasureHunters';
 describe('TreasureHunters - Full Expedition', () => {
     let blockchain: Blockchain;
     let deployer: SandboxContract<TreasuryContract>;
+    let referrer: SandboxContract<TreasuryContract>;
     let treasureHunters: SandboxContract<TreasureHunters>;
     const players: SandboxContract<TreasuryContract>[] = [];
     const numberOfPlayers = 100; // Total players for the expedition
     const playersPerExpedition = 20n; // Players per expedition
     const ticketPrice = toNano('10'); // Ticket price in TON
+    const discountTicketPrice = toNano('9'); // Ticket price in TON
     const treasurePercent = 70n; // Treasure percent
+    const referrerBonusPercent = 5n; // Referrer bonus percent
     var iterations = 0;
 
     beforeEach(async () => {
         blockchain = await Blockchain.create();
         deployer = await blockchain.treasury('deployer');
+        referrer = await blockchain.treasury('referrer');
         console.log(`Deployer Balance before the game: ${await deployer.getBalance()}`);
-        treasureHunters = blockchain.openContract(await TreasureHunters.fromInit({ $$type: 'Config', numberOfPlayers: playersPerExpedition, ticketPrice: ticketPrice, treasurePercent: treasurePercent, }));
+        treasureHunters = blockchain.openContract(await TreasureHunters.fromInit({ $$type: 'Config', numberOfPlayers: playersPerExpedition, ticketPrice: ticketPrice, treasurePercent: treasurePercent, discountTicketPrice: discountTicketPrice, referrerBonusPercent: referrerBonusPercent }));
         iterations++;
 
         // Deploy the contract
@@ -46,7 +50,7 @@ describe('TreasureHunters - Full Expedition', () => {
         }
     });
 
-    it('should buy all tickets and print balances', async () => {
+    it('should buy all tickets with normal price', async () => {
         const deployerBalanceBefore = await deployer.getBalance() - 1000000000000000n;
         console.log(`Deployer Balance before the game: ${fromNano(deployerBalanceBefore)} TON`);
         console.log(`Deployer Balance before the game: ${Number(deployerBalanceBefore) / 1000000000 * 5.5} USDT`);
@@ -91,4 +95,54 @@ describe('TreasureHunters - Full Expedition', () => {
         expect((await treasureHunters.getExpeditionLog()).values().length).toEqual(numberOfPlayers / 20);
         console.log(`Game log: `, (await treasureHunters.getExpeditionLog()).values().map((v) => `Loosers: ${v.losers.values()}, Winner: ${v.winners.values()}`));
     });
+
+    // it('should buy all tickets with discount', async () => {
+    //     const deployerBalanceBefore = await deployer.getBalance() - 1000000000000000n;
+    //     console.log(`Deployer Balance before the game: ${fromNano(deployerBalanceBefore)} TON`);
+    //     console.log(`Deployer Balance before the game: ${Number(deployerBalanceBefore) / 1000000000 * 5.5} USDT`);
+
+    //     for (const player of players) {
+    //         const buyResult = await treasureHunters.send(
+    //             player.getSender(),
+    //             {
+    //                 value: discountTicketPrice,
+    //             },
+    //             {
+    //                 $$type: 'BuyFirstTicketAfterReferral',
+    //                 referrer: referrer.address,
+    //             }
+    //         );
+
+    //         expect(buyResult.transactions).toHaveTransaction({
+    //             from: player.address,
+    //             to: treasureHunters.address,
+    //             success: true,
+    //         });
+    //     }
+
+    //     const withdrawalResult = await treasureHunters.send(
+    //         deployer.getSender(),
+    //         {
+    //             value: toNano('0.05'),
+    //         },
+    //         {
+    //             $$type: 'Withdraw',
+    //         }
+    //     );
+
+    //     expect(withdrawalResult.transactions).toHaveTransaction({
+    //         from: treasureHunters.address,
+    //         to: deployer.address,
+    //         success: true,
+    //     });
+
+    //     const deployerBalance = await deployer.getBalance() - 1000000000000000n;
+    //     console.log(`Deployer Balance after the game: ${fromNano(deployerBalance)} TON`);
+    //     console.log(`Deployer Balance after the game: ${Number(deployerBalance) / 1000000000 * 5.5} USDT`);
+    //     expect((await treasureHunters.getExpeditionLog()).values().length).toEqual(numberOfPlayers / 20);
+    //     console.log(`Game log: `, (await treasureHunters.getExpeditionLog()).values().map((v) => `Loosers: ${v.losers.values()}, Winner: ${v.winners.values()}`));
+    //     const referrerBalance = await referrer.getBalance();
+    //     console.log(`Referrer Balance after the game: ${fromNano(referrerBalance)} TON`);
+    //     expect(referrerBalance).toBeGreaterThan(1000000000000000n);
+    // });
 });
